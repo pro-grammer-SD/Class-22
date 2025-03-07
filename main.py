f2b11c0e03a2337d271e5cc1666d92f44d13fc91
pro-grammer-SD <geniussantu1983@gmail.com>
2025-03-07 18:04:44 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+student = (10,12,16,19,29,2,41,2,41,41,10,29,29,29)
+length_of_the_dayum_tuple = len(student)
+print(length_of_the_dayum_tuple)
+print("Sliced 🔪🩸 THE TUPLE (ok that was physically w fanum tax)")
+print(f"Now doing it programmatically: {student[2:6]}")
+print(f"{student + (9,)} -> new tuple")
+print(f"{student.count(41)}")
+# w elon musk(m[elon]) rocket 🔥
